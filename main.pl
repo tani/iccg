2ba05d342fa923303af74becdb32d211461b6fba
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-11-04 19:07:50 +0900

    run script parallel on SWI-Prolog]

--- main.pl
+++ main.pl
@@ -6,9 +6,30 @@
 :- if(current_prolog_flag(dialect, swi)).
 
 main :-
-  concurrent_forall(
-    (between(3, 5, I), length(G, I), maplist(axiom, G), cg(_, G, _), freeze(G)),
-    (parse(iccg, 32, G, _))
+  findall(
+    N,
+    (between(3, 6, I), length(G, I), maplist(axiom, G, N), cg(_, N, T), T=node('S' by _, _), freeze(G)),
+    NS
+  ),
+  concurrent_maplist(
+    parse(cg, 16),
+    NS,
+    TS
+  ),
+  concurrent_maplist(
+    parse(iqccg, 16),
+    NS,
+    US
+  ),
+  forall(
+    between(1, N, I),
+    (
+      nth1(I, TS, T),
+      nth1(I, US, U),
+      write_chars(80, '='),
+      draw_tree(T),
+      draw_tree(U)
+    )
   ),
   halt.
 
